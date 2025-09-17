0d134398582b907d4026b1b857003752c1155bb8
Elvis Raul <chavezelvis8122@gmail.com>
2025-09-17 16:59:19 -0500

    Avance 17-09-2025-16-59

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const listado = [];
+const padreActual = document.getElementById("listaAmigos");
+const seleccionLista = document.querySelectorAll("li");
+
+function agregarAmigo(){
+    
+    var deficionNombre = document.getElementById("amigo").value;
+    listado.push(deficionNombre);
+    alert(listado);
+
+    //limpieza de lista
+    seleccionLista.forEach(e => e.remove())
+
+    listado.forEach(e =>{
+        //console.log(e);
+        const nuevoDiv = document.createElement("li");
+        nuevoDiv.textContent = e;
+        padreActual.appendChild(nuevoDiv);
+    });
+
+    document.getElementById("amigo").value = "";
+    //document.getElementById("listaAmigos").innerHTML = listado;
+}
+
+
+function sortearAmigo(){
+    //limpieza de lista
+    seleccionLista.forEach(e => e.remove());
+  
+}
