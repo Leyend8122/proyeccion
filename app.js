0f811a9956c133581654bc225ad85a48a96bf686
Elvis Raul <chavezelvis8122@gmail.com>
2025-09-18 11:53:00 -0500

    funcionalidad de juego completada

--- app.js
+++ app.js
@@ -1,31 +1,56 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 const listado = [];
 const padreActual = document.getElementById("listaAmigos");
-const seleccionLista = document.querySelectorAll("li");
+var seleccionLista;
 
 function agregarAmigo(){
-    
     var deficionNombre = document.getElementById("amigo").value;
-    listado.push(deficionNombre);
-    alert(listado);
 
-    //limpieza de lista
-    seleccionLista.forEach(e => e.remove())
+    if(deficionNombre == ""){
+        alert("Se necesita Ingresar un nombre válido");
+    }else{
+        listado.push(deficionNombre.toUpperCase());
+        //limpieza de lista
+        this.limpiezaListado();
 
-    listado.forEach(e =>{
-        //console.log(e);
-        const nuevoDiv = document.createElement("li");
-        nuevoDiv.textContent = e;
-        padreActual.appendChild(nuevoDiv);
-    });
+        listado.forEach(e =>{
+            const nuevoDiv = document.createElement("li");
+            nuevoDiv.textContent = e;
+            padreActual.appendChild(nuevoDiv);
+        });
+        document.getElementById("amigo").value = "";
+    }
 
-    document.getElementById("amigo").value = "";
+    
+    
+
+
+    //seleccionLista.forEach(e => e.remove());
     //document.getElementById("listaAmigos").innerHTML = listado;
 }
+
+function limpiezaListado(){
+    seleccionLista = document.querySelectorAll("li");
+    seleccionLista.forEach(e => e.remove());
+};
 
 
 function sortearAmigo(){
     //limpieza de lista
-    seleccionLista.forEach(e => e.remove());
+    this.limpiezaListado();
+
+    if(listado.length != 0){
+        var indice = parseInt(Math.random() * (listado.length)) + 1;
+
+  
+
+        const nuevoDiv = document.createElement("li");
+        nuevoDiv.textContent =  listado[indice - 1] + " fuiste elegido como mi amigo secreto.";
+        padreActual.appendChild(nuevoDiv);
+
+        listado.length = 0;
+    }
+    
+        
   
 }
